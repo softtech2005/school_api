3129dbc9b699e2703f2a0632206364c8315b3509
softtech2005 <enginemustobey@gmail.com>
2025-12-15 14:14:39 +0100

    adding root endpoint

--- school_api/urls.py
+++ school_api/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({
+        "status": "ok",
+        "message": "School API is running"
+    })
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('students.urls')),
+    path("", home),                # ROOT endpoint
+    path("admin/", admin.site.urls),
+    path("api/", include("students.urls")),
 ]
